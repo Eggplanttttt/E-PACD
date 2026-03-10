d0f0053076784a849ab9d648aaee1d070991cc66
Eggplanttttt <158043669+Eggplanttttt@users.noreply.github.com>
2026-03-10 09:42:27 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -42,19 +42,19 @@ use Illuminate\Support\Facades\Mail;
 |--------------------------------------------------------------------------
 */
 
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+// use Illuminate\Session\Middleware\StartSession;
+// use Illuminate\View\Middleware\ShareErrorsFromSession;
+// use Illuminate\Cookie\Middleware\EncryptCookies;
+// use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
-Route::get('/health-no-session', function () {
-    return 'NO SESSION OK';
-})->withoutMiddleware([
-    EncryptCookies::class,
-    AddQueuedCookiesToResponse::class,
-    StartSession::class,
-    ShareErrorsFromSession::class,
-]);
+// Route::get('/health-no-session', function () {
+//     return 'NO SESSION OK';
+// })->withoutMiddleware([
+//     EncryptCookies::class,
+//     AddQueuedCookiesToResponse::class,
+//     StartSession::class,
+//     ShareErrorsFromSession::class,
+// ]);
 
 // Route::get('/', function () {
 //     return 'HOME OK';
