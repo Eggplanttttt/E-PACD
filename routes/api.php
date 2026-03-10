e8447feb45f1337ddde66a5d7efcd2dc58cade6e
Eggplanttttt <158043669+Eggplanttttt@users.noreply.github.com>
2026-03-10 09:30:03 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/health-test', function () {
+    return response()->json(['ok' => true]);
+});
