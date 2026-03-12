f6e93fed1b47d62ffdc4e4a556450f7456e1f6ac
Ezekiel Cadiz <158043669+Eggplanttttt@users.noreply.github.com>
2026-03-12 20:37:49 +0800

    Change 'General Login' to 'Guest Login' in form

--- resources/views/inquiries.blade.php
+++ resources/views/inquiries.blade.php
@@ -247,7 +247,7 @@
     <form id="othersLoginForm" class="contact-left hidden" method="POST" action="{{ route('client.login.post', ['clientType' => 'others']) }}">
         @csrf
         <div class="contact-left-title">
-            <h2>General Login</h2>
+            <h2>Guest Login</h2>
             <a type="button" class="back-btn" onclick="goBackToClientType()">
                 <i class="fa-solid fa-arrow-left"></i>
             </a>
@@ -294,7 +294,7 @@
                 <option value="student">Student</option>
                 <option value="faculty">Faculty / Staff</option>
                 <option value="alumni">Alumni</option>
-                <option value="others">Others</option>
+                <option value="others">Guest</option>
             </select>
              <i class="fa-solid fa-angle-down dropdown-arrow" style="right:125px"></i>
              </div>
