1533c9c7d7a8bac24b4559444223cc788ea7253f
Ezekiel Cadiz <158043669+Eggplanttttt@users.noreply.github.com>
2026-03-12 23:01:30 +0800

    Update Message.php

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -11,14 +11,26 @@ class Message extends Model
     
     protected $table = 'messages';
 
+    protected $appends = [
+        'attachment_url',
+    ];
+
    protected $fillable = [
         'client_type',
         'client_id',
         'message',
+        'attachment_path',
+        'attachment_name',
+        'attachment_mime',
+        'attachment_type',
         'sender',
         'is_read',
         'solved',
         'unread_for_admin',
     ];
-}
 
+    public function getAttachmentUrlAttribute(): ?string
+    {
+        return $this->attachment_path ? asset($this->attachment_path) : null;
+    }
+}
