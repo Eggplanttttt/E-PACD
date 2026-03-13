5163af96c3ac21f1258bc386632c3b0d18b36d81
Ezekiel Cadiz <158043669+Eggplanttttt@users.noreply.github.com>
2026-03-13 13:57:59 +0800

    Create data-deletion.blade.php

---
+++ resources/views/data-deletion.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Data Deletion Instructions</title>
+</head>
+<body style="font-family: Arial; max-width: 900px; margin:40px auto; line-height:1.7;">
+
+<h1>Data Deletion Instructions</h1>
+
+<p>If you wish to delete your data associated with the E-PACD system, you may request account deletion through the following methods:</p>
+
+<ul>
+<li>Contact the system administrator.</li>
+<li>Send an email request to the support contact listed on the website.</li>
+</ul>
+
+<p>Upon receiving the request, the administrator will verify the request and permanently remove the associated user data from the system.</p>
+
+<p>This process ensures that personal information is handled securely and in accordance with our privacy policies.</p>
+
+</body>
+</html>
